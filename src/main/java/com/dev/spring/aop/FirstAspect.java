ed0b723364171ecb248d73e8d09687b972c1d9e4
VladimirImir <mimimvvv@gmail.com>
2024-07-01 19:39:51 +0300

    Spring. 121. - AOP. JoinPoint. Params.

--- src/main/java/com/dev/spring/aop/FirstAspect.java
+++ src/main/java/com/dev/spring/aop/FirstAspect.java
@@ -1,10 +1,12 @@
 package com.dev.spring.aop;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Aspect
@@ -73,10 +75,19 @@ public class FirstAspect {
     public void anyFindByIdServiceMethod() {
     }
 
-    @Before("anyFindByIdServiceMethod()")
-//    @Before("execution(public * com.dev.spring.service.*Service.findById(*))")
-    public void addLogging() {
-        log.info("invoked findById method");
+    @Before(value = "anyFindByIdServiceMethod() " +
+                    "&& args(id) " +
+                    "&& target(service) " +
+                    "&& this(serviceProxy)" +
+                    "&& @within(transactional)",
+            argNames = "joinPoint,id,service,serviceProxy,transactional")
+//    @Before("execution(public * com.dmdev.spring.service.*Service.findById(*))")
+    public void addLogging(JoinPoint joinPoint,
+                           Object id,
+                           Object service,
+                           Object serviceProxy,
+                           Transactional transactional) {
+        log.info("invoked findById method in class {}, with id {}", service, id);
     }
 
 }
